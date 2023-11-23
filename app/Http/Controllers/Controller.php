973e9cbfc07e67d3d25fbfa30d9bd636b74d7173
AnaStar11 <estk11.bnbn@gmail.com>
2023-11-23 15:42:31 -0600

    fallidas

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,35 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function concatenar($cadena1, $cadena2)
+    {
+        return $cadena1 . $cadena2;
+    }
+
+    public function longitudCadena($cadena)
+    {
+        return strlen($cadena);
+    }
+
+    public function arraySuma($numeros)
+    {
+        return array_sum($numeros);
+    }
+
+    public function esPar($numero)
+    {
+        return $numero % 2 == 0;
+    }
+
+    public function arrayInvertir($array)
+    {
+        return array_reverse($array);
+    }
+
+
+
+
+
+
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,4 +13,61 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testConcatenar()
+    {
+        $resultado = (new \App\Http\Controllers\Controller)->concatenar('Hola', 'Mundo');
+
+        $this->assertEquals('HolaMundo', $resultado);
+        $this->assertIsString($resultado);
+        $this->assertNotEmpty($resultado);
+        $this->assertStringContainsString('Mundo', $resultado);
+        $this->assertSame(10, strlen($resultado));
+    }
+
+    public function testLongitudCadena()
+    {
+        $resultado = (new \App\Http\Controllers\Controller)->longitudCadena('Laravel');
+
+        $this->assertIsInt($resultado);
+        $this->assertGreaterThan(0, $resultado);
+        $this->assertEquals(7, $resultado);
+        $this->assertIsNotString($resultado);
+        $this->assertLessThanOrEqual(10, $resultado);
+    }
+
+    public function testArraySuma()
+    {
+        $resultado = (new \App\Http\Controllers\Controller)->arraySuma([1, 2, 3, 4, 5]);
+
+        $this->assertEquals(15, $resultado);
+        $this->assertIsInt($resultado);
+        $this->assertGreaterThanOrEqual(10, $resultado);
+        $this->assertLessThanOrEqual(20, $resultado);
+        $this->assertNotEquals(20, $resultado);
+    }
+
+    public function testEsPar()
+    {
+        $esPar = (new \App\Http\Controllers\Controller)->esPar(10);
+
+        $this->assertTrue($esPar);
+        $this->assertIsBool($esPar);
+        $this->assertNotFalse($esPar);
+        $this->assertFalse((new \App\Http\Controllers\Controller)->esPar(7));
+        $this->assertNotTrue((new \App\Http\Controllers\Controller)->esPar(7));
+    }
+
+    public function testArrayInvertir()
+    {
+        $resultado = (new \App\Http\Controllers\Controller)->arrayInvertir([1, 2, 3, 4, 5]);
+
+        $this->assertEquals([5, 4, 3, 2, 1], $resultado);
+        $this->assertNotEquals([1, 2, 3, 4, 5], $resultado);
+        $this->assertIsArray($resultado);
+        $this->assertCount(5, $resultado);
+        $this->assertContains(3, $resultado);
+    }
+
+
 }
